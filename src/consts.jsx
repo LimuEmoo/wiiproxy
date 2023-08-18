b21ff737aced6df526363721490ca2dd9cd9870b
emoo <oliver.mcmillan08@gmail.com>
2023-08-18 15:43:43 -0500

    build1

---
+++ src/consts.jsx
@@ -0,0 +1,6 @@
+const obfuscation = true;
+const github = "https://www.youtube.com/watch?v=xNIM453_dgg";
+const discord = "https://discord.gg/qsnTxGXYM6";
+const bareServerURL = new URL("/bare/", window.location);
+
+export { obfuscation, github, discord, bareServerURL };
